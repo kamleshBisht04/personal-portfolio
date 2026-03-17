1bd6a3b4a94b45cd1355ec4a316491e8da1463ae
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-03-17 22:47:10 +0530

    correct the error

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
       {/* Green dots */}
       <ParticlesBackground />
       {/* Content */}
-      <div className="container-custom relative z-10 mx-auto w-full pt-32 pb-20">
+      <div className="mx-auto max-w-screen-2xl px-4 pt-32 pb-20 sm:px-6 md:px-10 lg:px-16">
         <div className="grid items-center gap-12 lg:grid-cols-2">
           {/* left column -Text section */}
           <div className="space-y-8">
