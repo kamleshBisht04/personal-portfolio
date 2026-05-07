91fa2ddfcc279cd231297a5a42b0d71d37b2d533
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-05-07 09:50:43 +0530

    center the github calender in the profile section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from "@/layout/Navbar";
 import Hero from "@/sections/Hero";
 import About from "@/sections/About";
-import Contact from "@/sections/Contact";
+// import Contact from "@/sections/Contact";
 import Skills from "@/sections/Skills";
 import Projects from "@/sections/Projects";
 import Footer from "@/layout/Footer";
 import { Toaster } from "react-hot-toast";
 import GithubHeatmap from "./sections/GithubHeatmap";
+import Contact from "./sections/Contact";
 
 const App = () => {
   return (
