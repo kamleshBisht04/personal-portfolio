935f0c485d91f9e1b36decc2c08c316bb72f6745
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-05-07 09:34:34 +0530

    center the github calender in the profile section

--- src/sections/GithubHeatmap.jsx
+++ src/sections/GithubHeatmap.jsx
@@ -6,7 +6,7 @@ const GithubHeatmap = () => {
       <h1 className="text-center text-xl font-bold text-white/70 mb-6 md:text-2xl">
         GitHub Contributions
       </h1>
-      <div className="ml-[18rem] flex max-w-6xl items-center justify-center rounded-xl border bg-black/80 p-6">
+      <div className="mx-auto flex max-w-6xl items-center justify-center rounded-xl border bg-black/80 p-6">
         <GitHubCalendar username="kamleshBisht04" colorScheme="dark" />
       </div>
     </>
